726b4d9079040c413aa463dbf026e9657fca192f
Kyle Corales <kai.corales@gmail.com>
2014-11-21 13:44:19 -0600

    changed the remote server

--- www/js/app.js
+++ www/js/app.js
@@ -25,8 +25,8 @@ angular.module('terry-management-app', [
         }
     });
 
-    Restangular.setBaseUrl("http://localhost:8080/terrytest/");
-    //Restangular.setBaseUrl("http://www.housuggest.org:8080/terrytest/");
+    //Restangular.setBaseUrl("http://localhost:8080/terrytest/");
+    Restangular.setBaseUrl("http://www.housuggest.org:8080/terrytest/");
     //Restangular.setBaseUrl("http://www.housuggest.org:8080/terry/");
     
     $rootScope.Restangular = function () {
